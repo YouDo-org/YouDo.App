018f0c24ab91acfac91db866028bc54359c1fc2c
Sarper Makas <sarper.makas@superonline.com>
2023-07-12 19:38:02 +0300

    Blog conent show more feature

--- MVVM/Model/MainModels/BlogModels/BlogContentModel.cs
+++ MVVM/Model/MainModels/BlogModels/BlogContentModel.cs
@@ -1,30 +1,40 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using YouDo.MVVM.ViewModel;
 
 namespace YouDo.MVVM.Model.MainModels.BlogModels;
 
-public partial class BlogContentModel : BaseContentModel {
+public partial class BlogContentModel : BaseContentModel{
 
     [ObservableProperty]
     string blogContent = "";
+
+    string temp_BlogContent;
 
     [ObservableProperty]
     bool commentVisibility = false;
 
     [ObservableProperty]
-    public ObservableCollection<CommentModule> comments;
+    ObservableCollection<CommentModule> comments;
+
+    static int temp_maxLines = 3;
+
+    [ObservableProperty]
+    int maxLines = temp_maxLines;
+
+    int maxCharacters = 110;
+
+    private bool isExpanded = true;
 
     public BlogContentModel(string username, string userdefenition, string blogcontent, DateTime releasedate, string userIconPath) :
-        base(username, userdefenition, releasedate, userIconPath) {
+        base(username, userdefenition, releasedate, userIconPath)
+        {
         
-        blogContent = blogcontent;
+        temp_BlogContent = blogcontent;
+        blogContent = temp_BlogContent;
+        ExpandedContent();
 
         comments = new ObservableCollection<CommentModule> {
             new CommentModule("Alex", "Developer", "Nice Bro !!!", DateTime.Now, "userimage.png"),
@@ -32,9 +42,34 @@ public partial class BlogContentModel : BaseContentModel {
             new CommentModule("Jim", "Editor","Thanks", new DateTime(2023, 7, 10), "userimage.png")
         };
     }
+    
+    private void ExpandedContent() {
+        if (isExpanded) {
+            MaxLines = temp_maxLines;
+            if (temp_BlogContent.Length >= maxCharacters) {
+                BlogContent = temp_BlogContent.Substring(0, maxCharacters) + "...";
+            }
+        } else {
+            MaxLines = int.MaxValue;
+            BlogContent = temp_BlogContent;
+        }
+
+        isExpanded = !isExpanded;
+    }
+
 
     [RelayCommand]
     void ChangeCommentVisibility() {
         CommentVisibility = !CommentVisibility;
     }
+
+
+    [RelayCommand]
+    private void LabelTapped(Label contentLabel) {
+        ExpandedContent();
+        contentLabel.MaxLines = MaxLines;
+        contentLabel.Text = BlogContent;
+    }
+
+
 }
